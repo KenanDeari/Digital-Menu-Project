3f8b59d8443b59aafa2ba3e394ba84ffac301cab
vjeuel <vjeuel@gmail.com>
2020-07-05 10:15:51 -0500

    database started, gitinore file created

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+USE paraiso_lb_db;
+
+INSERT INTO menu (section, item, descrip, price)
+VALUES   ("Appetizers", 
+         "Brazilian Cheese Bread", 
+         "Gooey explosion of flavors, made with yucca flour, eggs and 5 different cheeses.",
+         10),
+         ("Appetizers",
+         "Aji Amarillo Hummus",
+         "Hummus served with aji amarillo sauce on the side, served with pita chips, carrots and celery.",
+         10)
