d509002a06aa12ddc34ac69a324ed2424fc572ed
vjeuel <vjeuel@gmail.com>
2020-07-14 22:07:58 -0500

    seeds file fixed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,144 +1,144 @@
 USE druid_db;
 
-INSERT INTO Paraiso (section, item, descrip, price, deleted, createdAt, updatedAt)
+INSERT INTO Paraiso (section, item, descrip, price, createdAt, updatedAt)
 VALUES  
 
     ("Appetizers",
     "Tamale",
     "Made with chicken or pork Peruvian style tamale served with pickeld onions.",
-    5, false, now(), now()),
+    5, now(), now()),
 
     ("Appetizers",
     "Papa a la Huancaina",
     "Potato slices on lettuce topped with a mild and creamy yellow sauce.",
-    8, false, now(), now()),
+    8, now(), now()),
 
     ("Appetizers",
     "Salchipapa",
     "Fries and sliced hot dogs topped with in-house sauces served with lettuce.",
-    8, false, now(), now()),
+    8, now(), now()),
 
     ("Appetizers",
     "Papa Rellena",
     "Fried potato croquette filled with ground beef, onions, black olives, raisins, and eggs.",
-    8, false, now(), now()),
+    8, now(), now()),
 
     ("Appetizers",
     "Causa Rellena con Pollo",
     "Seasoned mashed potato terrine stuffed with a chicken salad topped with mayo.",
-    8, false, now(), now()),
+    8, now(), now()),
 
     ("Entrees",
     "Lomo Saltado",
     "Stir fried beef with sautied tomatoes and onions served with fries and rice.",
-    14, false, now(), now()),
+    14, now(), now()),
 
     ("Entrees",
     "Seco de Carne",
     "Beef, potatoes and cilantro stew served with canary beans and rice",
-    15, false, now(), now()),
+    15, now(), now()),
 
     ("Entrees",
     "Aji de Gallina",
     "Pulled chicken on a Peruvian yellow pepper sauce served over potatos and rice.",
-    13, false, now(), now()),
+    13, now(), now()),
 
     ("Entrees",
     "Cau Cau",
     "Beef tripe served with potatoes on a Peruvian yellow pepper sauce served with rice.",
-    13, false, now(), now()),
+    13, now(), now()),
 
     ("Entrees",
     "Arroz Chaufa",
     "Peruvian styled fried rice served with chicken or beef.",
-    13, false, now(), now()),
+    13, now(), now()),
 
     ("Entrees",
     "Tallarines Verdes con Bistec",
     "Peruvian style pesto on spaghetti served with a thinly sliced steak.",
-    15, false, now(), now()),
+    15, now(), now()),
 
     ("Entrees",
     "Tallarines Verdes con Papa a la Huancaina",
     "Peruvian style pesto on spaghetti served with Papa a la Huancaina.",
-    13, false, now(), now()),
+    13, now(), now()),
 
     ("Entrees",
     "Pollo a la brasa con papas y ensalada",
     "Peruvian style rotisserie chicken served with fries and a house salad.",
-    20, false, now(), now()),
+    20, now(), now()),
 
     ("Sides",
     "Cancha",
     "Peruvian toasted corn.",
-    2, false, now(), now()),
+    2, now(), now()),
 
     ("Sides",
     "Fried Plantains",
     "Sliced plantains fried in oil",
-    3, false, now(), now()),
+    3, now(), now()),
 
     ("Sides",
     "Huancaina Sauce",
     "Sauce made from queso fresco (fresh white cheese), vegetable oil, ají amarillo (yellow Peruvian pepper), evaporated milk.",
-    3, false, now(), now()),
+    3, now(), now()),
 
     ("Sides",
     "Fries",
     "Sliced potatoes deep fried and lightly salted",
-    4, false, now(), now()),
+    4, now(), now()),
 
     ("Sides",
     "Fried Yucca",
     "Fried root vegatable.",
-    4, false, now(), now()),
+    4, now(), now()),
 
     ("Sides",
     "Side salad",
     "Mixture of lettuce, cucumber, tomatoes. Topped with a dressing of choice.",
-    4, false, now(), now()),
+    4, now(), now()),
     
     ("Beverages",
     "Chicha Morada",
     "Purple corn drink.",
-    3, false, now(), now()),
+    3, now(), now()),
 
     ("Beverages",
     "Maracuya",
     "Passion fruit drink.",
-    3, false, now(), now()),
+    3, now(), now()),
 
     ("Beverages",
     "Inca Kola, Coke (can)",
     "Soda",
-    2, false, now(), now()),
+    2, now(), now()),
 
     ("Desserts",
     "Alfajor",
     "Cookie sandwich filled with creamy caramel.",
-    3, false, now(), now()),
+    3, now(), now()),
 
     ("Desserts",
     "Crema Volteada",
     "Peruvian style creme caramel",
-    3, false, now(), now()),
+    3, now(), now()),
 
     ("Desserts",
     "Pionono",
     "Soft and light sponge cake filled with creamy caramel.",
-    3, false, now(), now()),
+    3, now(), now()),
 
     ("Desserts",
     "Torta Helada",
     "Slice of cake suspended within two layers of gelatin. (Chocolate or Vanilla)",
-    3, false, now(), now()),
+    3, now(), now()),
 
     ("Desserts",
     "Milhojas",
     "Layers of thin crispy puff pastry filled with creamy caramel.",
-    3, false, now(), now()),
+    3, now(), now()),
 
     ("Desserts",
     "Leche Asada",
     "Peruvian style flan with a toasted layer on the surface.",
-    3, false, now(), now())
+    3, now(), now())
